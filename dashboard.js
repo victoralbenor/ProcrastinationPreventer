7b16255ef4e9547f999c7380a67bdf05dc48826d
Victor Albenor <victoralbenor@gmail.com>
2023-04-12 21:38:26 -0300

    move dashboard to a separate page, fix extension name

--- dashboard.js
+++ dashboard.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
             const table = document.querySelector('#eventTable tbody');
             allowedSites.forEach(site => {
                 const row = table.insertRow();
-                row.insertCell().textContent = site.hostname;
                 row.insertCell().textContent = new Date(site.until - site.duration * 60 * 1000).toLocaleString();
+                row.insertCell().textContent = site.hostname;
                 row.insertCell().textContent = site.reason;
                 row.insertCell().textContent = site.duration;
             });
